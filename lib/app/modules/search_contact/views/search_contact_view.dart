503f06a3fd5f5c51ecfb6fbf1c7b5e18c8ceb86b
saipulanwarr <saipul0994@gmail.com>
2023-11-27 11:05:00 +0700

    search user

--- lib/app/modules/search_contact/views/search_contact_view.dart
+++ lib/app/modules/search_contact/views/search_contact_view.dart
@@ -7,36 +7,6 @@ import 'package:sachatapp/app/routes/app_pages.dart';
 import '../controllers/search_contact_controller.dart';
 
 class SearchContactView extends GetView<SearchContactController> {
-  final List<Widget> myChats = List.generate(
-    20,
-    (index) => ListTile(
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundColor: Colors.black26,
-        child: Image.asset(
-          "assets/logo/noimage.png",
-          fit: BoxFit.cover,
-        ),
-      ),
-      title: Text(
-        "Title ${index + 1}",
-        style: TextStyle(
-          fontWeight: FontWeight.w600,
-          fontSize: 20,
-        ),
-      ),
-      subtitle: Text(
-        "Subtitle ${index + 1}",
-        style: TextStyle(fontSize: 16),
-      ),
-      trailing: GestureDetector(
-        onTap: () => Get.toNamed(Routes.CHAT_ROOM),
-        child: Chip(
-          label: Text("Message"),
-        ),
-      ),
-    ),
-  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,6 +22,7 @@ class SearchContactView extends GetView<SearchContactController> {
               alignment: Alignment.bottomCenter,
               child: TextField(
                 controller: controller.searchC,
+                onChanged: (value) => controller.searchFriend(value),
                 cursorColor: Colors.red[900],
                 decoration: InputDecoration(
                   fillColor: Colors.white,
@@ -83,19 +54,47 @@ class SearchContactView extends GetView<SearchContactController> {
           ),
         ),
       ),
-      body: myChats.length == 0
-          ? Center(
-              child: Container(
-                width: Get.width * 0.7,
-                height: Get.width * 0.7,
-                child: Lottie.asset("assets/lottie/empty.json"),
+      body: Obx(
+        () => controller.tempSearch.length == 0
+            ? Center(
+                child: Container(
+                  width: Get.width * 0.7,
+                  height: Get.width * 0.7,
+                  child: Lottie.asset("assets/lottie/empty.json"),
+                ),
+              )
+            : ListView.builder(
+                padding: EdgeInsets.zero,
+                itemCount: controller.tempSearch.length,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    backgroundColor: Colors.black26,
+                    child: Image.asset(
+                      "assets/logo/noimage.png",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  title: Text(
+                    "${controller.tempSearch[index]["name"]}",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 20,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "${controller.tempSearch[index]["email"]}",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  trailing: GestureDetector(
+                    onTap: () => Get.toNamed(Routes.CHAT_ROOM),
+                    child: Chip(
+                      label: Text("Message"),
+                    ),
+                  ),
+                ),
               ),
-            )
-          : ListView.builder(
-              itemCount: myChats.length,
-              itemBuilder: (context, index) => myChats[index],
-              padding: EdgeInsets.zero,
-            ),
+      ),
     );
   }
 }
